4914217315d6bd58f1314b380329cda3c57a2953
Xdellta <66367896+Xdellta@users.noreply.github.com>
2022-12-18 00:51:58 +0100

    -

---
+++ Scripts/trends.js
@@ -0,0 +1,79 @@
+fetch('Scripts/products.json').then(response => response.json()).then(data => {
+    let product1 = data[0];
+    let product2 = data[0];
+    let product3 = data[0];
+
+    for (let product of data) {
+        if (product.Opinion > product1.Opinion) {
+            product1 = product;
+        }
+
+        if (product.Opinion > product2.Opinion && product.Opinion < product1.Opinion) {
+            product2 = product;
+        }
+
+        if (product.Opinion > product3.Opinion && product.Opinion < product2.Opinion) {
+            product3 = product;
+        }
+    }
+
+
+    // Product #1
+    const trendsItem2 = document.getElementById('trends_item2');
+
+    trendsItem2.innerHTML = `
+        <div>
+            <i class="fa-solid fa-coffee-beans"></i>
+            <img src="Images/Products/${product1.Graphic}" alt="">
+
+            <div class="trends_item_opinion">
+                ${product1.Opinion}<i class="fa-solid fa-star"></i>
+            </div>
+        </div>
+        <h1>${product1.Name}</h1>
+        <button>
+            Do koszyka
+            <span>+</span>
+        </button>
+    `;
+
+
+    // Product #2
+    const trendsItem3 = document.getElementById('trends_item3');
+
+    trendsItem3.innerHTML = `
+        <div>
+            <i class="fa-solid fa-coffee-beans"></i>
+            <img src="Images/Products/${product2.Graphic}" alt="">
+
+            <div class="trends_item_opinion">
+                ${product2.Opinion}<i class="fa-solid fa-star"></i>
+            </div>
+        </div>
+        <h1>${product2.Name}</h1>
+        <button>
+            Do koszyka
+            <span>+</span>
+        </button>
+    `;
+
+
+    // Product #3
+    const trendsItem1 = document.getElementById('trends_item1');
+
+    trendsItem1.innerHTML = `
+        <div>
+            <i class="fa-solid fa-coffee-beans"></i>
+            <img src="Images/Products/${product3.Graphic}" alt="">
+
+            <div class="trends_item_opinion">
+                ${product3.Opinion}<i class="fa-solid fa-star"></i>
+            </div>
+        </div>
+        <h1>${product3.Name}</h1>
+        <button>
+            Do koszyka
+            <span>+</span>
+        </button>
+    `;
+});
