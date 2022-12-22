582f98786e1ba24a1986f5288b1fdd8a1b4bf29a
Xdellta <66367896+Xdellta@users.noreply.github.com>
2022-12-22 22:15:55 +0100

    -

--- Scripts/nav.js
+++ Scripts/nav.js
@@ -5,3 +5,21 @@ const searchBtn = document.getElementById('searchBtn');
 searchBtn.addEventListener('click', function() {
     console.log(searchInput.value);
 })
+
+
+// Burger menu worker
+const burgerIcon = document.getElementById('burgerIcon');
+const menu = document.getElementById('nav_menu');
+
+burgerIcon.addEventListener('click',function() {
+    burgerIcon.classList.toggle('menuActive');
+    menu.classList.toggle('menuActive');
+
+    // Remove menu if width > 1150px
+    window.addEventListener('resize',function() {
+        if (window.innerWidth > 1150) {
+            burgerIcon.classList.remove('menuActive');
+            menu.classList.remove('menuActive');
+        }
+    })
+})
